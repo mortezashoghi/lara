656d5f619e6bf670173c418a2a78bc978f51a359
mortezashoghi <mortaza.shoghi@gmail.com>
2023-10-09 15:54:35 +0330

    test fasade and service provider roll back

--- app/Test/testfacades.php
+++
@@ -1,8 +0,0 @@
-<?php
-   namespace App\Test;
-   class TestFacades{
-      public function testingFacades() {
-         echo "Testing the Facades in Laravel.";
-      }
-   }
-?>
--- config/app.php
+++ config/app.php
@@ -168,7 +168,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\App\Providers\facadestestserviceprovider::class,
+        #App\App\Providers\facadestestserviceprovider::class,
     ])->toArray(),
 
     /*
